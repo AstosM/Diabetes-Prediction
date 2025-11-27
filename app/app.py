61a43d0803bcebbcf4f3f064e59cb68ad2ec1160
AstosM <astosm123@gmail.com>
2025-11-27 23:25:38 +0530

    app.py

--- app/app.py
+++ app/app.py
@@ -34,7 +34,7 @@ test_acc = accuracy_score(Y_test, classifier.predict(X_test))
 # -------------------------------
 st.set_page_config(page_title="Diabetes Prediction App", page_icon="🩺", layout="wide")
 
-st.title("🩺 Diabetes Prediction App by ASHUTOSH & ISHA")
+st.title("🩺 Diabetes Prediction App by ASHUTOSH ")
 st.markdown("I'm ASHUTOSH , made a simple ML app using **Support Vector Machine (SVM)** to predict diabetes.")
 
 # Sidebar
