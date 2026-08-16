64003ea248bd8d12cd3f651fb8a703d2b78e566f
AstosM <astosm123@gmail.com>
2026-08-16 14:14:52 +0530

    SyntheticLab.jsx

--- frontend/src/components/SyntheticLab.jsx
+++ frontend/src/components/SyntheticLab.jsx
@@ -76,7 +76,7 @@ function SyntheticLab() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://diabetes-prediction-m6o4.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
