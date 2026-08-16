17bc8eccba3b5f56bd3b25c927d944c819826847
AstosM <astosm123@gmail.com>
2026-08-16 14:55:32 +0530

    main.py
    Updated CORS middleware to include the deployed frontend URL.

--- backend/main.py
+++ backend/main.py
@@ -12,9 +12,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        # Add your deployed frontend URL here after deployment
-        # Example:
-        # "https://your-frontend.onrender.com",
+         "https://diabetes-prediction-6zwp.onrender.com/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
