401a9069392da8fa77e22d91ccd8c041717093bc
AstosM <astosm123@gmail.com>
2026-08-16 14:11:41 +0530

    Prediction.jsx
    Updated the prediction service URL to use the deployed endpoint.

--- frontend/src/components/Prediction.jsx
+++ frontend/src/components/Prediction.jsx
@@ -37,7 +37,7 @@ function Prediction() {
     setResult(null);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://diabetes-prediction-m6o4.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
